e26aeefbde76577337c0db8d9d5f8eaaeb263225
Rafael Armenta <rarmenta@hawk.iit.edu>
2021-10-19 21:30:11 -0500

    added javascript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+function init(){
+
+	document.getElementById("submit").addEventListener("click", function()
+	{
+		var output = "Thank You for signing up to the Speedwagon Foundation Newsletter. Be sure to check your email for an exclusive discount.";
+		alert(output);
+	});
+
+}
+window.addEventListener('load', init);
+
 // jQuery 3.x-style ready event and locally scoped $
 jQuery(function($) {
   $('html').removeClass('nojs');
