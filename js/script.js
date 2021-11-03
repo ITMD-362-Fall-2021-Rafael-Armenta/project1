9cc40dd732e5be4382af82a8d318ba7f27064aeb
Rafael Armenta <rarmenta@hawk.iit.edu>
2021-11-02 23:44:48 -0500

    fonts + cascade
    added an imported google font and fixed the cascade errors

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 function init(){
 
-	document.getElementById("submit").addEventListener("click", function()
-	{
-		var output = "Thank You for signing up to the Speedwagon Foundation Newsletter. Be sure to check your email for an exclusive discount.";
-		alert(output);
-	});
-
+  document.getElementById("submit").addEventListener("click", function()
+  {
+    var output = "Thank You for signing up to the Speedwagon Foundation Newsletter. Be sure to check your email for an exclusive discount.";
+	alert(output);
+  });
 }
+
 window.addEventListener('load', init);
 
 // jQuery 3.x-style ready event and locally scoped $
